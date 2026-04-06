5ef20e5a787d696520b94070252117371e85fca3
AlvinQID <github@alvinq.my.id>
2026-04-06 10:24:19 +0700

    Use NC API (#1)
    * fix or test!
    
    * fix or test!
    
    * fix or test!

--- xmake.lua
+++ xmake.lua
@@ -1,27 +1,21 @@
-set_project("EnchantLimitLess")
+set_project("EnchantUnbound")
 set_version("1.0.0")
 
 set_languages("cxx23")
 
 add_rules("mode.release")
 
-add_repositories(
-    "xmake-repo https://github.com/xmake-io/xmake-repo.git"
-)
+add_cxflags("-O2", "-fvisibility=hidden", "-ffunction-sections", "-fdata-sections", "-flto", "-w")
+add_ldflags("-Wl,--gc-sections", "-Wl,--strip-all", "-s")
+
+add_repositories("xmake-repo https://github.com/xmake-io/xmake-repo.git")
 
 add_requires("nlohmann_json v3.11.3")
 
-target("EnchantLimitLess")
+target("EnchantUnbound")
     set_kind("shared")
-    add_packages("nlohmann_json")
-    add_linkdirs("niseAPI/libs/arm64-v8a")
-    add_linkdirs("miniAPI/libs/arm64-v8a")
-    add_links("nise", "log", "miniAPI", "GlossHook")
-
-    add_files("src/*.cpp", "niseAPI/deps/gamepwnage/src/*.c", "niseAPI/deps/gamepwnage/src/**/*.c")
-
-    add_includedirs("miniAPI/include", "niseAPI/include", "niseAPI/deps/gamepwnage/includes", {public = true})
-
-    add_cxflags("-O2", "-fvisibility=hidden", "-ffunction-sections", "-fdata-sections", "-w")
-    add_cflags("-O2", "-fvisibility=hidden", "-ffunction-sections", "-fdata-sections", "-w")
-    add_ldflags("-Wl,--gc-sections,--strip-all", "-s")
+    add_files("src/main.cpp")
+    add_linkdirs("libs/arm64-v8a")
+    add_links("log", "android", "dl")
+    add_links("nexcaiseAPI", {kind = "static"})
+    add_includedirs("include", {public = true})

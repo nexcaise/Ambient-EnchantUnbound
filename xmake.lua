b6412bb22796b05c3c9f36d34ce695997d699019
alvinqid <github@alvinq.my.id>
2026-04-06 12:08:49 +0700

    fix or test!

--- xmake.lua
+++ xmake.lua
@@ -5,9 +5,6 @@ set_languages("cxx23")
 
 add_rules("mode.release")
 
-add_cxflags("-O2", "-fvisibility=hidden", "-ffunction-sections", "-fdata-sections", "-flto", "-w")
-add_ldflags("-Wl,--gc-sections", "-Wl,--strip-all", "-s")
-
 add_repositories("xmake-repo https://github.com/xmake-io/xmake-repo.git")
 
 add_requires("nlohmann_json v3.11.3")
